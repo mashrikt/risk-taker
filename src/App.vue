451c393171279135571d70695e8693af308fe5ae
Tahzib Mashrik <mashrikt@gmail.com>
2019-05-21 04:22:33 +0600

    Setup basic GET and POST method to backend API

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,14 @@
 <template>
   <div id="app">
+
   <Header />
-  <Form />
+  <Form
+  :name="name"
+  :text="text"
+  :number="number"
+  :date="date"
+  :choice="choice"
+  />
   </div>
 </template>
 
@@ -14,8 +21,35 @@ export default {
   components: {
     Header,
     Form
+  },
+
+  data() {
+  return {
+    resource_id: 1,
+    name: "",
+    text: {},
+    number: {},
+    date: {},
+    choice: {}
+    }
+  },
+  mounted: function() {
+    fetch('http://157.230.35.253:3333/api/v1/risk-types/1/', {
+      method: 'get'
+    })
+    .then((response) => {
+        return response.json()
+      })
+      .then((jsonData) => {
+
+        this.name = jsonData.name
+        this.text = jsonData.risk_fields[0]
+        this.number = jsonData.risk_fields[1]
+        this.date = jsonData.risk_fields[2]
+        this.choice = jsonData.risk_fields[3]
+      })
   }
-}
+};
 </script>
 
 <style>
--- src/components/Form.vue
+++ src/components/Form.vue
@@ -1,26 +1,27 @@
 <template>
   <div>
     <b-col sm="6" offset="3">
-      <b-form @submit="onSubmit">
-
+      <h3>{{name}}</h3>
+      <hr>
+      <b-form  @submit="onSubmit">
         <b-form-group label-cols="4" label-cols-lg="2" label="Name" label-for="input-1">
-          <b-form-input id="input-1" size="sm" required></b-form-input>
+          <b-form-input id="input-1" size="sm" v-model="post_data.name" required></b-form-input>
         </b-form-group>
 
-        <b-form-group label-cols="4" label-cols-lg="2" label="Text" label-for="input-3">
-          <b-form-input id="input-1" size="sm" required></b-form-input>
+        <b-form-group label-cols="4" label-cols-lg="2" :label=text.display_name label-for="input-2">
+          <b-form-input id="input-1" size="sm" v-model="post_data.data.description" required></b-form-input>
         </b-form-group>
 
-        <b-form-group label-cols="4" label-cols-lg="2" label="Number" label-for="input-5">
-          <b-form-input id="input-1" size="sm" type="number" required></b-form-input>
+        <b-form-group label-cols="4" label-cols-lg="2" :label=number.display_name label-for="input-3">
+          <b-form-input id="input-1" size="sm" type="number" v-model="post_data.data.no_of_hair" required></b-form-input>
         </b-form-group>
 
-        <b-form-group label-cols="4" label-cols-lg="2" label="Date" label-for="input-7">
-          <b-form-input id="input-1" size="sm" type="date" required></b-form-input>
+        <b-form-group label-cols="4" label-cols-lg="2" :label=date.display_name label-for="input-4">
+          <b-form-input id="input-1" size="sm" type="date" v-model="post_data.data.dob" required></b-form-input>
         </b-form-group>
 
-        <b-form-group label-cols="4" label-cols-lg="2" label="Choice" label-for="input-9">
-          <b-form-select :options="options"></b-form-select>
+        <b-form-group label-cols="4" label-cols-lg="2" :label=choice.display_name label-for="input-5">
+          <b-form-select :options=choice.choices v-model="post_data.data.hair_color" required></b-form-select>
         </b-form-group>
 
         <b-button type="submit" variant="primary">Submit</b-button>
@@ -31,13 +32,46 @@
 
 <script>
   export default {
-    data() {
-      return {
-        options: [
-          { value: 'a', text: 'a' },
-          { value: 'b', text: 'b' }
-        ]
-      }
+    props: {
+      name: String,
+      text: Object,
+      number: Object,
+      date: Object,
+      choice: Object,
+    },
+    data () {
+        return {
+            post_data: {
+                risk_type: 1,
+                name: '',
+                data: {
+                  description: '',
+                  no_of_hair: null,
+                  dob: '',
+                  hair_color: ''
+                }
+            }
+        }
+    },
+    methods: {
+      onSubmit(evt) {
+        evt.preventDefault()
+        fetch('http://157.230.35.253:3333/api/v1/risks/', {
+          method: 'post',
+          headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify(this.post_data)
+        })
+        .then((response) => {
+          alert("Response Status: " + response.status)
+          return response.json()
+        })
+        .then((jsonData) => {
+          alert("Response Data: " + JSON.stringify(jsonData))
+        })
+      },
     }
   }
 </script>
